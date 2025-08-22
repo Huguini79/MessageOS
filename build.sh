4bfed7abea26d675229d3147d53936251ff78377
Huguini79 <166934655+Huguini79@users.noreply.github.com>
2025-08-22 05:39:27 +0100

    Add files via upload

--- build.sh
+++ build.sh
@@ -1,11 +1,11 @@
 clear
 nasm -f elf32 src/kernel/kernel.asm -o build/kernel.asm.o
 nasm -f elf32 src/drivers/io/io.asm -o build/io.asm.o
-g++ -I./src -m32 -c src/kernel/kernel.cpp -o build/kernel.o
-g++ -I./src -m32 -c src/drivers/vga/vga.cpp -o build/vga.o
-g++ -I./src -m32 -c src/drivers/memory/memory.cpp -o build/memory.o
-g++ -I./src -m32 -c src/drivers/keyboard/keyboard.cpp -o build/keyboard.o
-g++ -I./src -m32 -c src/drivers/disk/disk.cpp -o build/disk.o
+g++ -I./src -m32 -ffreestanding -nostdlib -fno-exceptions -fno-rtti -c src/kernel/kernel.cpp -o build/kernel.o
+g++ -I./src -m32 -ffreestanding -nostdlib -fno-exceptions -fno-rtti -c src/drivers/vga/vga.cpp -o build/vga.o
+g++ -I./src -m32 -ffreestanding -nostdlib -fno-exceptions -fno-rtti -c src/drivers/memory/memory.cpp -o build/memory.o
+g++ -I./src -m32 -ffreestanding -nostdlib -fno-exceptions -fno-rtti -c src/drivers/keyboard/keyboard.cpp -o build/keyboard.o
+g++ -I./src -m32 -ffreestanding -nostdlib -fno-exceptions -fno-rtti -c src/drivers/disk/disk.cpp -o build/disk.o
 ld -m elf_i386 -T src/linker/linker.ld -o iso/boot/kernel build/kernel.asm.o build/kernel.o build/vga.o build/io.asm.o build/memory.o build/keyboard.o build/disk.o
 grub-mkrescue -o bin/MessageOS.iso iso
 qemu-system-x86_64 -m 1024 bin/MessageOS.iso
